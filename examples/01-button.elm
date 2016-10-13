99e3d6816b421cba3492d1d628ddddd2a100f4aa
Conner D <connerdunn7399@gmail.com>
2016-10-12 21:23:32 -0600

    Deleted whitespace & swapped +/- btns

--- examples/01-button.elm
+++ examples/01-button.elm
@@ -2,8 +2,6 @@ import Html exposing (Html, button, div, text)
 import Html.App as Html
 import Html.Events exposing (onClick)
 
-
-
 main =
   Html.beginnerProgram
     { model = model
@@ -12,26 +10,20 @@ main =
     }
 
 
-
 -- MODEL
 
-
 type alias Model = Int
-
 
 model : Model
 model =
   0
 
 
-
 -- UPDATE
-
 
 type Msg
   = Increment
   | Decrement
-
 
 update : Msg -> Model -> Model
 update msg model =
@@ -43,14 +35,12 @@ update msg model =
       model - 1
 
 
-
 -- VIEW
-
 
 view : Model -> Html Msg
 view model =
   div []
-    [ button [ onClick Decrement ] [ text "-" ]
+    [ button [ onClick Increment ] [ text "+" ]
     , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick Decrement ] [ text "-" ]
     ]
